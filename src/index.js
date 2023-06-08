ec42b43cfd33543ec2e243d681f8b34df53111f1
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-06-08 20:01:48 +0600

    Add form functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
 import _ from 'lodash';
 import './style.css';
 
+// let cloud = 'cloud';
+let array = JSON.parse(localStorage.getItem('Data')) || [];
+const listInput = document.querySelector('#listInput');
+let counter = 1;
+const list = document.querySelector('#list');
 
+const updateArray = (counter) => {
+  counter++;
+  array.push({
+    index: counter,
+    description: listInput.value,
+    completed: false,
+  });
+  localStorage.setItem('Data', JSON.stringify(array));
+};
 
+listInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter' && listInput.value) {
+    updateArray(listInput.value);
+    listInput.value = '';
 
+    showList();
+  }
+});
 
-// import Icon from './icon.png';
+let showList = () => {
+  list.innerHTML = '';
+  array.forEach((task, index) => {
+    const taskContainer = document.createElement('li');
+    taskContainer.classList.add('taskContainer');
 
-// function component() {
-//     const element = document.createElement('div');
-  
-//     // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
-  
-//     // Add the image to our existing div.
-//     const myIcon = new Image();
-//     myIcon.src = Icon;
-  
-//     element.appendChild(myIcon);
-  
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('check');
+
+    const taskNote = document.createElement('input');
+    taskNote.type = 'text';
+    taskNote.classList.add('taskNote');
+    taskNote.value = task.description;
+
+    const edit = document.createElement('i');
+    edit.className = 'fa-solid fa-pen-to-square';
+
+    const remove = document.createElement('i');
+    remove.className = 'fa-solid fa-trash';
+
+    list.appendChild(taskContainer);
+    taskContainer.appendChild(checkbox);
+    taskContainer.appendChild(taskNote);
+    taskContainer.appendChild(edit);
+  });
+};
+showList();
